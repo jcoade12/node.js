03faa3622fba73e550e6b2162dabf10f0aa44391
jcoade12 <117692495+jcoade12@users.noreply.github.com>
2024-01-29 02:23:03 -0330

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,11 @@
+// global_example.js
+
+// Using global variables
+global.myVariable = 'global variable';
+console.log(myVariable);
+
+// Accessing global functions
+global.myFunction = function () {
+  console.log('global function');
+};
+myFunction();
---
+++ Untitled-2.js
@@ -0,0 +1,12 @@
+// simple adding code
+
+//my fuinction too add two numbers
+function addNumbers(a,b){
+
+    return a+b;
+}
+
+//logging my result
+const result =addNumbers(5,7);
+
+console.log('5 and 7 is:', result);
